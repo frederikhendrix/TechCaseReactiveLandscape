da3e4446fe758136232bef1cb2622fb6c1101b58
FrederikHendrix <rikhendrix@live.nl>
2023-02-16 13:21:33 +0100

    Initial Commit

---
+++ TechCaseReactiveLandscape/TechCaseReactiveLandscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechCaseReactiveLandscapeApp.swift
+//  TechCaseReactiveLandscape
+//
+//  Created by Rik Hendrix on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TechCaseReactiveLandscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
