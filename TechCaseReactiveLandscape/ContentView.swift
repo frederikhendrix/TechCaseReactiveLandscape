dc2c5ae18cb7e3cf79995bf1c91e413738e76ccb
FrederikHendrix <rikhendrix@live.nl>
2023-03-15 11:07:35 +0100

    Added sounds changes to display

--- TechCaseReactiveLandscape/ContentView.swift
+++ TechCaseReactiveLandscape/ContentView.swift
@@ -10,38 +10,42 @@ import CoreMotion
 
 struct ContentView: View {
     
-    @ObservedObject var player = AudioPlayer(name: "KickDrumLoop", type: "aif", volume: 0.6758675867587)
+    @ObservedObject var player = AudioPlayer(name: "KickDrumLoop", type: "aif")
     
+    @State private var selection = "KickDrumLoop"
+    
+    let sounds = ["KickDrumLoop", "ChimeMelodyLoop", "RingFirstFourLoop", "RingLastFourLoop", "ShakerLoop", "SilenceLoop", "SineMelodyLoop", "SinePulseLoop", "SnareDrumLoop", "SynthPulseLoop"]
     
     let movementManager = CMMotionManager()
     
     @State var speedVolume: Float = 0
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Button(action:{
-                self.player.player.play()
-                print("button is pressed")
-            }){
-                Text("play audio")
-            }.foregroundColor(.blue).background(.white)
-            Button(action:{
-                movementManager.startAccelerometerUpdates()
-                if let accelerometerData = movementManager.accelerometerData {
-                    player.playerAudio(name: "KickDrumLoop", type: "aif", volume: Float(accelerometerData.acceleration.x))
-                    print("this is the speed data \(accelerometerData.acceleration.x)")
-                    speedVolume = Float(accelerometerData.acceleration.x)
+        VStack{
+            VStack {
+                Picker("Select a paint color", selection: $selection) {
+                        ForEach(sounds, id: \.self){
+                            Text($0)
+                        }
                 }
-            }){
-                Text("Movement Meter")
+                .pickerStyle(.menu)
+                Button(action:{
+                    movementManager.startAccelerometerUpdates()
+                    if let accelerometerData = movementManager.accelerometerData {
+                        player.playerAudio(name: selection, type: "aif", volume: Float(accelerometerData.acceleration.x))
+                        print("this is the speed data \(accelerometerData.acceleration.x)")
+                        speedVolume = Float(accelerometerData.acceleration.x)
+                    }
+                    print(selection)
+                }){
+                    Text("Play Audio")
+                }
+                            
+                
             }
-                        
             Text("Volume of sound is set to \(speedVolume)")
         }
+        
         .padding()
     }
 }
